3b41365cd8cb3305874b0dd39a624df8594a7b30
gh0sty02 <pranayyadav-cmpn@atharvacoe.ac.in>
2021-07-01 22:20:00 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ mongoose
     }
   )
   .then(() => {
-    app.listen(process.env.PORT || 5000);
+    app.listen(process.env.PORT);
     console.log("server connected successfully");
   })
   .catch((err) => console.log(err));
