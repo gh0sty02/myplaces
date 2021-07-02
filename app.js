8cd003ff6eadc2372da930c2807e9cdf1bb955f5
gh0sty02 <pranayyadav-cmpn@atharvacoe.ac.in>
2021-07-02 10:54:54 +0530

    deploy done

--- app.js
+++ app.js
@@ -16,15 +16,15 @@ app.use(express.json());
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 app.use(express.static(path.join("public")));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//   next();
+// });
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", userRoutes);
@@ -33,10 +33,10 @@ app.use((req, res, next) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
-app.use((req, res, next) => {
-  const error = new HttpError("cannot find this route", 404);
-  throw error;
-});
+// app.use((req, res, next) => {
+//   const error = new HttpError("cannot find this route", 404);
+//   throw error;
+// });
 
 app.use((error, req, res, next) => {
   if (req.file) {
